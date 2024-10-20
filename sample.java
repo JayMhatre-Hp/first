158d0beced80de753a27373e668c4116d4a0e8e2
JayMhatre-Hp <jaypramodmhatre@gmail.com>
2024-10-20 19:50:52 +0530

    sample.java

--- sample.java
+++ sample.java
@@ -1,4 +1,4 @@
-public class Test {
+public class sample {
      public static void main(String[] args){
 	       System.out.println("********************************");
 		   System.out.println("Hello From GITHUB");
